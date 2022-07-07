bef7be474e8346b148247e6bc9844762096addc6
Vinicius Pacheco <vinicioss9955@gmail.com>
2022-07-07 20:29:27 -0300

    POST cart completed

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { getHome } from '../controllers/userControllers.js'
+import { getHome, postGame } from '../controllers/userControllers.js'
+import validateUser from '../middlewares/authMiddlewares/validadeUser.js'
 
 const router = Router()
 
 router.get('/games', getHome)
+router.post('/cart/:id', validateUser, postGame)
 
 export default router
