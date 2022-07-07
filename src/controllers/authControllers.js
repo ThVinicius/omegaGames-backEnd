b16a02ffd49763d4491a9739fe0e100836b2150d
nando-castro <fernando.castro10.master@gmail.com>
2022-07-07 20:39:25 -0300

    update token user session

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,13 +1,12 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { db,objectId } from "../db/mongo.js";
+import { db, objectId } from "../db/mongo.js";
 import loginSchema from "../schemas/authShemas/loginSchema.js";
-
 
 export async function signIn(req, res) {
   const { email, password } = req.body;
 
-  const { error } = loginSchema.validate({email,password});
+  const { error } = loginSchema.validate({ email, password });
 
   if (error) {
     return res.sendStatus(422);
@@ -23,24 +22,29 @@ export async function signIn(req, res) {
     const comparePassword = bcrypt.compareSync(password, user.password);
 
     if (user && comparePassword) {
-      const token = jwt.sign(
-        { id: user._id },
-        process.env.JWT_SECRET,
-        {
-          expiresIn: "1d",
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
+
+      const sessionUser = await db
+        .collection("sessions")
+        .findOne({ userId: user._id });
+
+      if (sessionUser) {
+        try {
+          await db
+            .collection("sessions")
+            .updateOne({ userId: user._id }, { $set: { token: token } });
+          return res.status(400).send(`usuário já logado ${token}`);
+        } catch (error) {
+          console.log(error);
+          res.sendStatus(404);
         }
-      );
-
-      /* const sessionUser = await db.collection("sessions").findOne({ id: objectId(user._id)});
-
-      if(sessionUser.id){
-          //atualiza user
-          res.status(400).send("usuario já existe");
-      } */
+      }
 
       await db.collection("sessions").insertOne({
         userId: objectId(user._id),
-        token
+        token,
       });
 
       return res.status(201).send({ token });
@@ -52,26 +56,26 @@ export async function signIn(req, res) {
     res.sendStatus(500);
   }
 }
-export async function register (req, res) {
+export async function register(req, res) {
   try {
-    const { name, email, picture, password } = req.body
+    const { name, email, picture, password } = req.body;
 
-    const cryptPassword = bcrypt.hashSync(password, 10)
+    const cryptPassword = bcrypt.hashSync(password, 10);
 
     const toSend = {
       name,
       email,
       picture,
       password: cryptPassword,
-      cart: [],
-      purchases: []
-    }
+      cart: [{ id: objectId, name, url, price }],
+      purchases: [],
+    };
 
-    await db.collection('users').insertOne(toSend)
+    await db.collection("users").insertOne(toSend);
 
-    return res.sendStatus(201)
+    return res.sendStatus(201);
   } catch (error) {
-    console.log(error)
-    return res.status(500).send(error)
+    console.log(error);
+    return res.status(500).send(error);
   }
 }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { signIn, register } from "../controllers/authControllers.js";
-import registerValidate from '../middlewares/authMiddlewares/registerValidate.js';
+import registerValidate from "../middlewares/authMiddlewares/registerValidate.js";
 
 const router = Router();
 
 router.post("/login", signIn);
-router.post('/register', registerValidate, register)
+router.post("/register", registerValidate, register);
 
 export default router;
